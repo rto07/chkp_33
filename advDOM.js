d99e67ed30cba66d764780b5bf59e46b251d77b6
Raul Torres <rto07@users.noreply.github.com>
2018-06-01 16:03:30 -0500

    Updated advDOM.js
    6/1/18 4pm

--- advDOM.js
+++ advDOM.js
@@ -1,18 +1,29 @@
-/*Using the code from the prior checkpoint, add an Event Handler to the <li> element and console.log() the name of the shirt they selected.*/
+const li = ['listItem1', 'listItem2', 'listItem3'];
 
-let li = document.getElementById('listItem1')
-let button = document.createElement('button')
-button.innertext = 'Mens T-Shirt'
-listItem1.prepend(button)
+for (let i=0; i< li.length; i++){
+  if(document.getElementById('listItem1').onclick=()=>{
+ console.log("Biker Jacket") 
+})
+if(document.getElementById('listItem2').onclick=()=>{
+ console.log("Men's T-Shirt") 
+})
+  if(
+document.getElementById('listItem3').onclick=()=>{
+	console.log("Men's Shirt")
+});
+};
 
-const btnB =
-        document.getElementById('btnB');
-  btnB.onclick = () => {
-    console.log("Bicker Jacket");
-  };
+let buttonA = 
+		document.getElementById('buttonA')    
+		document.createElement('button')
+		buttonA.innerText = "Biker Jacket"
 
-const btnC =
-        document.getElementById('btnC');
-  btnC.onclick = () => {
-    console.log("Men's Shirt");
-  };
+let buttonB = 
+	document.getElementById('buttonB')    
+	document.createElement('button')
+	buttonB.innerText = "Men's T-Shirt"
+
+let buttonC = 
+	document.getElementById('buttonC')    
+	document.createElement('button')
+	buttonC.innerText = "Men's Shirt"
